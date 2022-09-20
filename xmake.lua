db80e4c6c238be2a11815b111195a45931b2dbea
Your Name <you@example.com>
2022-09-20 11:45:51 +0800

    modify xmake.lua

--- xmake.lua
+++ xmake.lua
@@ -1,13 +1,23 @@
-set_defaultarchs("linux|x64")
+-- set_defaultarchs("linux|x64")
 
 lua_version=os.getenv("LUAVER")
 
 add_requires("lua " .. lua_version)
 
-target("main")
+-- target("linux-x64")
+--     set_kind("binary")
+--     add_rules("mode.debug")
+--     add_files("loadfunctions.cpp", "main.cpp")
+--     add_packages("lua")
+-- target_end()
+
+
+target("dydeluac")
     set_kind("binary")
     add_rules("mode.debug")
-    -- set_toolchains("ndk", {plat = "android", arch = "arm64", ndk = "/home/ihopenot/android-ndk-r23b", sdkver="24"})
+    set_arch("linux")
+    -- set_arch("armeabi-v7a")
+    -- set_toolchains("ndk", {plat = "android", arch = "armeabi-v7a", ndk = "/home/ihopenot/android-ndk-r23b", sdkver="24"})
     add_files("loadfunctions.cpp", "main.cpp")
     add_packages("lua")
 target_end()
